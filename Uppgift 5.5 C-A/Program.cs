34449c4061637d8b913e22976f8495f7a32052b0
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 18:59:10 +0100

    Add project files.

---
+++ Uppgift 5.5 C-A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] svar = new string[10] { "Ja", "Nej", "Kanske", "Troligt", "Inte troligt", "Sällan sant", "Oftast sant", "sant", "icke sant", "maybe" };
+            Random slump = new Random();
+            while (true)
+            {
+                Console.WriteLine("Ställ en fråga, skriv \"a\" för att avsluta:");
+                string x = Console.ReadLine();
+                if (x != "a")
+                {
+                    string slumpmässigt = svar[slump.Next(10)];
+                    Console.WriteLine(slumpmässigt);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
